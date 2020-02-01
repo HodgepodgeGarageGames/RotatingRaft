0487d9b5568f2d225882d2c0ab914ed846835833
Micah Cowan <micah@addictivecode.org>
2020-02-01 03:49:37 -0800

    Player prefab, position rotates

---
+++ Boat/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(-1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Boat/Assets/Scripts/RaftBehavior.cs
+++ Boat/Assets/Scripts/RaftBehavior.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class RaftBehavior : MonoBehaviour
 {
-    public float    spinPerSecond = 30;
+    public float            angularVelocity = 30;
+    private Rigidbody2D     raftBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //// Set up physics body-based rotation
+        //raftBody = transform.Find("Raft").GetComponent<Rigidbody2D>();
+        //raftBody.angularVelocity = -spinPerSecond;
+        //raftBody.angularDrag = 0f;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float time = Time.fixedDeltaTime;
-        transform.Rotate(0f, 0f, time*-spinPerSecond);
+        transform.Rotate(0f, 0f, time*-angularVelocity);
+        var playBeh = transform.Find("PlayersAssembly").GetComponent<PlayersAssemblyBehavior>();
+        playBeh.Straighten();
     }
 }
