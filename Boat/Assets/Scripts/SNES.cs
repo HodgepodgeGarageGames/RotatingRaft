91c907dc43fbf3ca017ce4a27901d5815bf1097c
HodgepodgeGarageGames <hodgepodgegaragegames@gmail.com>
2020-02-01 16:08:06 -0800

    SNES

---
+++ Boat/Assets/Scripts/SNES.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SNES
+{
+    public class GamePad
+    {
+        private string horizontal;
+        private string vertical;
+        private string a;
+
+        public GamePad(string H, string V, string A)
+        {
+            horizontal = H;
+            vertical = V;
+            a = A;
+        }
+
+        public float Horizontal()
+        {
+            if (Input.GetAxis(horizontal) > 0.0f)
+                Debug.Log(horizontal);
+
+            return Input.GetAxis(horizontal);
+        }
+
+        public float Vertical()
+        {
+            if (Input.GetAxis(vertical) > 0.0f)
+                Debug.Log(vertical);
+
+            return Input.GetAxis(vertical);
+        }
+
+        public bool A()
+        {
+            if (Input.GetButton(a))
+                Debug.Log(a);
+
+            return Input.GetButton(a);
+        }
+
+        public bool ADown()
+        {
+            if (Input.GetButtonDown(a))
+                Debug.Log(a + " down");
+
+            return Input.GetButtonDown(a);
+        }
+
+        public bool AUp()
+        {
+            if (Input.GetButtonUp(a))
+                Debug.Log(a + " up");
+
+            return Input.GetButtonUp(a);
+        }
+    }
+
+    public static GamePad[] gamePad = {
+        new GamePad("P1Horizontal", "P1Vertical", "P1A"),
+        new GamePad("P2Horizontal", "P2Vertical", "P2A"),
+        new GamePad("P3Horizontal", "P3Vertical", "P3A"),
+        new GamePad("P4Horizontal", "P4Vertical", "P4A")
+    };
+}
---
+++ Boat/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SNES.gamePad[0].Horizontal();
+        SNES.gamePad[1].Horizontal();
+        SNES.gamePad[2].Horizontal();
+        SNES.gamePad[3].Horizontal();
+
+        SNES.gamePad[0].Vertical();
+        SNES.gamePad[1].Vertical();
+        SNES.gamePad[2].Vertical();
+        SNES.gamePad[3].Vertical();
+
+        SNES.gamePad[0].A();
+        SNES.gamePad[1].A();
+        SNES.gamePad[2].A();
+        SNES.gamePad[3].A();
+    }
+}
