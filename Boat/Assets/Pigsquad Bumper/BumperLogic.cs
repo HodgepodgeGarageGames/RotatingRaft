e8b9e178dbe4e0967083166a14d297bef8308938
HodgepodgeGarageGames <hodgepodgegaragegames@gmail.com>
2020-02-02 14:41:53 -0800

    PIGSquad bumper

---
+++ Boat/Assets/Pigsquad Bumper/BumperLogic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class BumperLogic : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer vp = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(playVid());
+    }
+
+    private IEnumerator playVid()
+    {
+        vp.Play();
+
+        while (!vp.isPlaying)
+            yield return null;
+
+        while (vp.isPlaying)
+            yield return null;
+
+        SceneManager.LoadScene("TitleScreen", LoadSceneMode.Single);
+    }
+}
