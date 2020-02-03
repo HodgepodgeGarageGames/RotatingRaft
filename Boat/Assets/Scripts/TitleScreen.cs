8d2a5de2eee866503fa66543300c28327f51e7f2
HodgepodgeGarageGames <hodgepodgegaragegames@gmail.com>
2020-02-02 17:10:37 -0800

    Title Screen Instructions

--- Boat/Assets/Scripts/TitleScreen.cs
+++ Boat/Assets/Scripts/TitleScreen.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class TitleScreen : MonoBehaviour
 {
+    [SerializeField] private GameObject instructions = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GlobalGameData.playersIn[0] = false;
+        GlobalGameData.playersIn[1] = false;
+        GlobalGameData.playersIn[2] = false;
+        GlobalGameData.playersIn[3] = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (GlobalGameData.playersIn[0] ||
+            GlobalGameData.playersIn[1] ||
+            GlobalGameData.playersIn[2] ||
+            GlobalGameData.playersIn[3])
+        {
+            instructions.SetActive(true);
+        }
+        else
+        {
+            instructions.SetActive(false);
+        }
     }
 }
